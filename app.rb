ce993c7fb6834a11c95224d94793bd7455d1cda2
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-01-26 16:32:57 +0900

    use jquery-mobile 1.4.0 instead of boostrap 3.0.3

--- app.rb
+++ app.rb
@@ -44,79 +44,55 @@ __END__
 %html(lang="ja")
  %head
   %meta(charset="utf-8")
-  %meta(name="viewport" content="width=device-width, initial-scale=1.0")
+  %meta(name="viewport" content="width=device-width, initial-scale=1")
   %title= @title
-  %link(rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/3.0.3/united/bootstrap.min.css")
-  %script(src="//code.jquery.com/jquery-2.1.0.min.js")
+  %link(rel="stylesheet" href="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.css")
+  %script(src="http://code.jquery.com/jquery-1.9.1.min.js")
+  %script(src="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js")
+
   :javascript
    jQuery(function($) {
-     jQuery.fn.autogrow = function(options) {
-       var handler, self, settings, timerId;
-       settings = $.extend({
-         extraLineHeight: 15,
-         timeoutBuffer: 100
-       }, options);
-       self = this;
-       timerId = self.data('timerId');
-       if (timerId) {
-         self.removeData('timerId');
-         clearTimeout(timerId);
-       }
-       handler = function() {
-         self.each(function(i, e) {
-           var clientHeight, scrollHeight;
-           scrollHeight = e.scrollHeight;
-           clientHeight = e.clientHeight;
-           if (clientHeight < scrollHeight) {
-             $(e).height(scrollHeight + settings.extraLineHeight);
-           }
-         });
-         self.removeData('timerId');
-       };
-       self.data('timerId', setTimeout(handler, settings.timeoutBuffer));
+     var handler = function() {
+       var src = $('#rd');
+       $.ajax({
+         url: "/rd2markdown",
+         type: "POST",
+         data: {
+           rd: src.val()
+         },
+         error: function(xhr, status, error) {
+           $('#markdown').val(error).change();
+         },
+         success: function(data, status, xhr) {
+           $('#markdown').val(data).change();
+         }
+       });
+       src.removeData('timerId');
      };
-     $(document).on('keyup.autogrow change.autogrow input.autogrow paste.autogrow', 'textarea', function() {
-       $(this).autogrow();
-     });
-     $('textarea').autogrow();
-
      $(document).on('keyup change input paste', '#rd', function() {
-       var handler, self, timerId;
-       self = $(this);
-       timerId = self.data('timerId');
+       var src, timerId;
+       src = $(this);
+       timerId = src.data('timerId');
        if (timerId) {
-         self.removeData('timerId');
+         src.removeData('timerId');
          clearTimeout(timerId);
        }
-       handler = function() {
-         $.ajax({
-           url: "/rd2markdown",
-           type: "POST",
-           data: {
-             rd: self.val()
-           },
-           error: function(xhr, status, error) {
-             $('#markdown').val(error).change();
-           },
-           success: function(data, status, xhr) {
-             $('#markdown').val(data).change();
-           }
-         });
-         self.removeData('timerId');
-       };
-       self.data('timerId', setTimeout(handler, 1000));
+       src.data('timerId', setTimeout(handler, 1000));
      });
 
    });
  %body.container
-  %h1= @title
-  %div= yield
-  .row
-   .col-md-6
-    %form(method="post" role="form")
-     .form-group
-      %textarea.form-control(rows="10" id="rd" name="rd")= @rd
-   .col-md-6
-    %form
-     .form-group
-      %textarea.form-control#markdown(readonly)
+  %div(data-role="page")
+   %div(data-role="header")
+    %h1= @title
+   %div(role="main" class="ui-content")
+    %div= yield
+    .ui-grid-a.ui-responsive
+     .ui-block-a
+      %form(method="post" role="form")
+       %label(for="rd") Input (RD):
+       %textarea.form-control(rows="10" id="rd" name="rd" placeholder="RD")= @rd
+     .ui-block-b
+      %form
+       %label(for="markdown") Output (Markdown):
+       %textarea.form-control(rows="10" id="markdown" name="markdown" placeholder="markdown")
