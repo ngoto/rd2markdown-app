3a58b9e93a087e28f943e64f909474bcb046dabf
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-01-26 13:05:29 +0900

    add autogrow

--- app.rb
+++ app.rb
@@ -45,6 +45,38 @@ __END__
   %meta(name="viewport" content="width=device-width, initial-scale=1.0")
   %title= @title
   %link(rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/3.0.3/united/bootstrap.min.css")
+  %script(src="//code.jquery.com/jquery-2.1.0.min.js")
+  :javascript
+   jQuery(function($) {
+     jQuery.fn.autogrow = function(options) {
+       var handler, self, settings, timerId;
+       settings = $.extend({
+         extraLineHeight: 15,
+         timeoutBuffer: 100
+       }, options);
+       self = this;
+       timerId = self.removeData('timerId');
+       if (timerId) {
+         clearTimeout(timerId);
+       }
+       handler = function() {
+         self.each(function(i, e) {
+           var clientHeight, scrollHeight;
+           scrollHeight = e.scrollHeight;
+           clientHeight = e.clientHeight;
+           if (clientHeight < scrollHeight) {
+             return $(e).height(scrollHeight + settings.extraLineHeight);
+           }
+         });
+         return self.removeData('timerId');
+       };
+       return self.data('timerId', setTimeout(handler, settings.timeoutBuffer));
+     };
+     $(document).on('keyup.autogrow change.autogrow input.autogrow paste.autogrow', 'textarea', function() {
+       return $(this).autogrow();
+     });
+     return $('textarea').autogrow();
+   });
  %body.container
   %h1= @title
   %div= yield
@@ -52,7 +84,7 @@ __END__
    .col-md-6
     %form(method="post" role="form")
      .form-group
-      %textarea.form-control(row="10" name="rd")= @rd
+      %textarea.form-control(rows="10" name="rd")= @rd
      %input.btn.btn-block.btn-default(type="submit")
    .col-md-6
     %pre.markdown&= @markdown
