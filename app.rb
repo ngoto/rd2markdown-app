8e76f5f118283072edefd51bc04282e75c7262ab
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-02-24 00:15:35 +0900

    update jquery-mobile to 1.4.1

--- app.rb
+++ app.rb
@@ -54,9 +54,9 @@ __END__
   %meta(charset="utf-8")
   %meta(name="viewport" content="width=device-width, initial-scale=1")
   %title= @title
-  %link(rel="stylesheet" href="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.css")
-  %script(src="http://code.jquery.com/jquery-1.9.1.min.js")
-  %script(src="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js")
+  %link(rel="stylesheet" href="http://code.jquery.com/mobile/1.4.1/jquery.mobile-1.4.1.min.css")
+  %script(src="http://code.jquery.com/jquery-1.10.2.min.js")
+  %script(src="http://code.jquery.com/mobile/1.4.1/jquery.mobile-1.4.1.min.js")
 
   :javascript
    jQuery(function($) {
